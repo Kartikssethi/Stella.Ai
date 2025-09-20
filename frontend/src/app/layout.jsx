0f1c7028d34b174fda806b0b0295dac9886c55fd
Yash <78804783+yashd-dev@users.noreply.github.com>
2025-09-21 01:36:52 +0530

    feat: added logo

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -35,6 +35,12 @@ export default function RootLayout({ children }) {
         lexend.variable,
       )}
     >
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✏️</text></svg>"
+        />
+      </head>
       <body className="flex h-full flex-col">{children}</body>
     </html>
   )
